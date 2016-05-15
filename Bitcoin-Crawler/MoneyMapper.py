62a240f78e1aaaab58ec2927181dd6b501d6f410
Mathieu Lavoie <mathieulavoie020@gmail.com>
2016-05-15 15:33:36 -0400

    Add Money Mapper

---
+++ Bitcoin-Crawler/MoneyMapper.py
@@ -0,0 +1,77 @@
+import sys
+import AddressUtils
+
+__author__ = 'Mathieu'
+import BlockchainCrawler
+import Settings
+from pymongo import MongoClient
+
+class MoneyMapper(BlockchainCrawler.BlockchainCrawler):
+
+    def __init__(self):
+        super().__init__()
+        self.money_movements = []
+        self.addr_utils = AddressUtils.Addressutils()
+        self.max_batch_insert = 10000
+
+    def do_work(self,inputs, outputs):
+        if len(inputs) == 0: #No Valid Tx, an empty block with only one mining tx
+            return
+        try:
+            source = self.addr_utils.convert_hash160_to_addr(self.addr_utils.convert_public_key_to_hash160(inputs[0]))
+            for out in outputs:
+                dest = self.addr_utils.get_hash160_from_CScript(out.scriptPubKey)
+                entry = {'block_id':self.block_id,'source_n_id':-1,'source':source,'destination_n_id':-1,'destination':dest,'amount':(out.nValue/100000000)}
+                self.money_movements.append(entry)
+        except Exception:
+            #print("Unable to parse Tx for Money : %s" %  repr(outputs))
+            return
+
+
+    def insert_into_db(self):
+        if len(self.money_movements) == 0:
+            print("Warning: no money movements to insert. Aborting.")
+            return
+
+        client = MongoClient(Settings.db_server, Settings.db_port)
+        db = client.bitcoin_db
+        collection = db.transactions
+        collection.insert_many(self.money_movements)
+        client.close()
+        print("DB Sync Finished")
+
+
+
+
+def start(start_block_id):
+        mapper = MoneyMapper()
+        block_id = start_block_id
+
+        try:
+            while mapper.crawl_block(block_id):
+                print("Money of Block %d mapped" % block_id)
+
+                if block_id - start_block_id > 0 and (block_id - start_block_id) % Settings.block_crawling_limit == 0:
+                    mapper.insert_into_db()
+                    mapper.money_movements = []
+
+                block_id+=1
+
+            #Sync the rest
+            mapper.insert_into_db()
+            mapper.money_movements = []
+
+            #DONE!
+
+        #For Debugging purpose
+        except:
+            input("An exception will rise ")
+            raise
+
+
+if __name__ == "__main__":
+    mapper = MoneyMapper()
+    if len(sys.argv) == 2 :
+        start(int(sys.argv[1]))
+    else:
+        print("Usage: python %s  <starting block id>" % sys.argv[0])
